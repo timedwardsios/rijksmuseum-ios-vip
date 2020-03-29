ee7dc7b56f30e747d40e9f635e4f68a584aa1f3c
Timothy Edwards <10256125+timedwardsios@users.noreply.github.com>
2020-03-29 14:40:30 +0200

    Remove unused TypeAlias

--- Utils/Utils/Web/WebSession.swift
+++ Utils/Utils/Web/WebSession.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 public protocol WebSession {
-    typealias DataTaskPublisher = Foundation.URLSession.DataTaskPublisher
     func dataTaskPublisher(for urlRequest: URLRequest) -> Foundation.URLSession.DataTaskPublisher
 }
 
